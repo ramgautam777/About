5312556b743b3ec49fd1f61e2f9fed386cca1d78
Shyam <harijanram483@gmail.com>
2025-01-16 20:13:13 +0530

    message

---
+++ nav-web/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar/>      
+      <div className='content'>
+        <h2> Welcome to my website!</h2>
+    </div>
+    </div>
+  );
+}
+
+export default App;
